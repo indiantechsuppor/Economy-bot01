66c85f967f413d1423f3f0aff354361829620e3e
indiantechsuppor <77151621+indiantechsuppor@users.noreply.github.com>
2021-01-08 11:48:53 +0000

    Create botConfig.js

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "Nzk3MDU3OTIxOTQxNzY2MTU1.X_g8CQ.DYpjECn-Py3qrkNTH3SZHgow9zU",
+    prefix: "%",
+    admins: [
+        "705444618652614726"
+],
+    debug: true,
+    countChannel: "countChannelID"
+};
